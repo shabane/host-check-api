6e17be2f7f889876e5864d3c3ed7d68c88bd1e4d
shabane <m.mohamadshabane@gmail.com>
2023-08-17 15:06:48 +0330

    [feat](add new var for Node detail)

--- checkhost_prj/settings.py
+++ checkhost_prj/settings.py
@@ -122,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Node server detailes.
+NODE = {
+    'name': '...'
+    ,'ip': '...'
+    ,'location': '...'
+    ,'data_center': '...'
+    ,'sub_domain': '...'
+    ,'other': '...'
+}
