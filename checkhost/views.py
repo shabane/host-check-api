92d3f0f61dd3b05ed2892de6733fe397ced9ffd9
shabane <m.mohamadshabane@gmail.com>
2023-08-17 14:57:35 +0330

    [feat](write an api to get host and port to check with core.tcp_check)

--- checkhost/views.py
+++ checkhost/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import views
+from django.http import HttpResponse
+from . import core
+from rest_framework.response import Response
 
-# Create your views here.
+
+class CheckTCP(views.APIView):
+    def get(self, request):
+        if request.GET.get('host') and request.GET.get('port'):
+            __host = request.GET['host']
+            __port = request.GET['port']
+            _ = core.is_tcp_open(__host, __port)
+            return Response({
+                'result': _,
+                'node': '...'
+            })
