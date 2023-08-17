860e30bd3d66ea2b1b8ebef8260c35177de4ebed
shabane <m.mohamadshabane@gmail.com>
2023-08-17 15:28:11 +0330

    [feat](get timeout from user endpoint)

--- checkhost/views.py
+++ checkhost/views.py
@@ -8,9 +8,11 @@ from checkhost_prj.settings import NODE
 class CheckTCP(views.APIView):
     def get(self, request):
         if request.GET.get('host') and request.GET.get('port'):
+            __timeout = request.GET.get('timeout') if request.GET.get('timeout') else 5
+            __timeout = int(__timeout) if __timeout.isdigit() else core.err("timeout should be integer")
             __host = request.GET['host']
             __port = request.GET['port']
-            _ = core.is_tcp_open(__host, __port)
+            _ = core.is_tcp_open(__host, __port, __timeout)
             return Response({
                 'result': _,
                 'node': NODE
