424c57c7b8f56ccfd7e156bf8d0a22b27ab0394e
shabane <m.mohamadshabane@gmail.com>
2023-08-17 14:55:57 +0330

    [feat](install rest_framework)

--- checkhost_prj/settings.py
+++ checkhost_prj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x($s)_-qe!)ce)+^g!=jv10rq4i7peaek01k(*#%tov62!@r2m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
